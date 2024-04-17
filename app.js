1d01b45b1f1d1194eee0e1e270ee2a69272244b1
Gustavo Lozada <zelote57@gmail.com>
2024-04-17 01:15:37 -0500

    first upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const fs = require('fs');
+const userName = 'Gus'
+
+fs.writeFile('user-data.txt', 'Nombre: ' + userName, 
+(err)=> {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log("File created");
+});
